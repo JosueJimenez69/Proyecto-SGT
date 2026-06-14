04c4f6c9c27af13df79b6be32d5bfe3269962a29
josuSGT <andrerodas5741727@gmail.com>
2026-06-14 18:13:58 -0300

    feat: configura soporte ASGI para tiempo real

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,22 @@
 """
-ASGI config for config project.
+Configuración ASGI del proyecto SGT.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+Este archivo permite que Django soporte WebSockets
+y funcionalidades en tiempo real mediante Channels.
 """
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
+# Define el archivo principal de configuración.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+# Aplicación Django para peticiones HTTP.
+django_asgi_app = get_asgi_application()
+
+# Router principal ASGI.
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+})
