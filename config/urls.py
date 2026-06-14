b86841cc5d27d3155235843e7685fdad6fe45e77
josuSGT <andrerodas5741727@gmail.com>
2026-06-14 18:09:10 -0300

    feat: configura rutas principales del proyecto

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,20 @@
 """
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Configuración principal de URLs del proyecto SGT.
+Aquí se conectan las rutas generales del sistema, el panel admin
+y las rutas internas de cada aplicación.
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    # Ruta del panel administrativo de Django.
     path('admin/', admin.site.urls),
+
+    # Rutas de autenticación de Django.
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Rutas de la app de tableros.
+    path('', include('boards.urls')),
 ]
