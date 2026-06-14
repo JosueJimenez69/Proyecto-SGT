9c20196bb7ffe6504e5f71264c63095f489f6579
Alesola <alejandro.solalinde5@gmail.com>
2026-06-14 20:53:17 -0300

    feat:Agrega RegisterForm para gestionar el registro de usuarios en el sistema

---
+++ apps/accounts/forms.py
@@ -0,0 +1,25 @@
+"""
+Formularios de autenticación.
+"""
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    """
+    Formulario de registro.
+    """
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
