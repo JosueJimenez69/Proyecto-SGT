cd2544d0d2c8d1f4879f1c981298c6f04fa8ef7d
Alesola <alejandro.solalinde5@gmail.com>
2026-06-17 21:01:54 -0300

    feat(boards): implementa dashboard protegido para usuarios autenticados

--- apps/boards/views.py
+++ apps/boards/views.py
@@ -1,3 +1,18 @@
+"""
+Vistas principales del sistema.
+"""
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    """
+    Dashboard principal.
+    """
+
+    return render(
+        request,
+        "boards/dashboard.html"
+    )
